9375a46d46a2fd1bb8877142634cf43daf4d70d8
earlvhin <earlgabuat@gmail.com>
2023-02-25 19:17:23 +0800

    Ticket Operator Sign In, Generic Components

--- src/app/modules/sign-in/components/sign-in-form/sign-in-form.component.ts
+++ src/app/modules/sign-in/components/sign-in-form/sign-in-form.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-sign-in-form',
-  templateUrl: './sign-in-form.component.html',
-  styleUrls: ['./sign-in-form.component.scss']
+    selector: 'app-sign-in-form',
+    templateUrl: './sign-in-form.component.html',
+    styleUrls: ['./sign-in-form.component.scss'],
 })
 export class SignInFormComponent {
-
+    signInForm: FormGroup = new FormGroup({
+        userId: new FormControl(''),
+        passCode: new FormControl(''),
+    });
 }
--- src/app/modules/sign-in/sign-in.module.ts
+++ src/app/modules/sign-in/sign-in.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
 import { SignInFormComponent } from './components/sign-in-form/sign-in-form.component';
 import { SIGNIN_ROUTES } from './sign-in-routing.module';
+import { GenericCardComponent } from '@components/generic-card';
+import { ButtonComponent } from '@components/button';
+
+const IMPORTED_COMPONENTS = [ButtonComponent, GenericCardComponent];
+const MODULE_COMPONENTS = [SignInPageComponent, SignInFormComponent];
 
 @NgModule({
-    declarations: [SignInPageComponent, SignInFormComponent],
-    imports: [CommonModule, RouterModule.forChild(SIGNIN_ROUTES)],
+    declarations: [IMPORTED_COMPONENTS, MODULE_COMPONENTS],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule.forChild(SIGNIN_ROUTES)],
     exports: [],
 })
 export class SignInModule {}
